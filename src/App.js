3f392f9c19045a7752d1ff1fc99a7056de398870
Sam Reinhard <SamReinhard@Sams-MacBook-Pro.local>
2019-04-29 22:01:54 -0500

    created initial logic to check the guess against the user's previous guesses in the round

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import Wrapper from "./components/Wrapper/wrapper";
 import images from "./images.json";
 import "./App.css";
 
+let guesses = [];
+
 class App extends Component {
   state= {
     images
@@ -11,6 +13,20 @@ class App extends Component {
 
   guess = id => {
     console.log(id);
+    // push the id into an array
+    guesses.push(id);
+    console.log("Guesses: " + guesses);
+    // check to see if it's already in the array
+    for (var i = 1; i < guesses.length; i++){
+      // if it is, reset score to 0 and alert the player that they lost
+      if (id === guesses[i - 1]){
+        alert("you already guessed that");
+        guesses = [];
+      }
+    }
+    
+    // if it isn't, score++ and alert the player that they guessed right
+    // shuffle the images
   };
 
   render(){
