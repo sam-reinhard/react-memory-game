9b82719ed9f5faf75bc1ef3a8677073802f77fa5
Sam Reinhard <SamReinhard@Sams-MacBook-Pro.local>
2019-04-30 22:45:53 -0500

    Updates

---
+++ precache-manifest.2891ed8e508eab8ef30151ba71caaf66.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b359e8d5d81d27e94b1b2f42d572abd",
+    "url": "/react-memory-game/index.html"
+  },
+  {
+    "revision": "c9faae5c7104e65ee60c",
+    "url": "/react-memory-game/static/css/main.0e0a3f2b.chunk.css"
+  },
+  {
+    "revision": "dd0ed99a380f8d225736",
+    "url": "/react-memory-game/static/js/2.884c28c3.chunk.js"
+  },
+  {
+    "revision": "c9faae5c7104e65ee60c",
+    "url": "/react-memory-game/static/js/main.c94c6e8c.chunk.js"
+  },
+  {
+    "revision": "e8b71f681a914c951703",
+    "url": "/react-memory-game/static/js/runtime~main.83a7a993.js"
+  },
+  {
+    "revision": "977e3fd0f430205e51e65488c11fdddc",
+    "url": "/react-memory-game/static/media/topography.977e3fd0.png"
+  }
+]);
